9a927eaa28ceb0a77b6c275f072bd322d026669c
Franz Wimmer <info@codefoundry.de>
2017-10-21 18:09:28 +0200

    Move type check to method + add unit test

--- LegacyWrapperClient/Client/WrapperClientFactory.cs
+++ LegacyWrapperClient/Client/WrapperClientFactory.cs
@@ -18,23 +18,19 @@ namespace LegacyWrapperClient.Client
     /// <typeparam name="TFunctions">Interface type to use for calls to the DLL.</typeparam>
     public static class WrapperClientFactory<TFunctions> where TFunctions : class, IDisposable
     {
-        static WrapperClientFactory()
+        /// <summary>
+        /// Creates a new instance of TFunctions.
+        /// All calls will be proxied over a named pipe to the wrapper executable.
+        /// </summary>
+        /// <param name="targetArchitecture">Architecture of the library to load (X86 / AMD64). Defaults to X86.</param>
+        /// <returns>Returns a new instance of TFunctions.</returns>
+        public static TFunctions CreateWrapperClient(TargetArchitecture targetArchitecture = TargetArchitecture.X86)
         {
             if (!typeof(TFunctions).IsInterface)
             {
                 throw new ArgumentException("Generic parameter type <TFunctions> must be an interface.", nameof(TFunctions));
             }
-        }
 
-        /// <summary>
-        /// Creates a new instance of TFunctions.
-        /// All calls will be proxied over a named pipe to the wrapper executable.
-        /// </summary>
-        /// <param name="libraryName">Name of the library to load.</param>
-        /// <param name="targetArchitecture">Architecture of the library to load (X86 / AMD64). Defaults to X86.</param>
-        /// <returns>Returns a new instance of TFunctions.</returns>
-        public static TFunctions CreateWrapperClient(TargetArchitecture targetArchitecture = TargetArchitecture.X86)
-        {
             IProxyGenerator generator = new ProxyGenerator(new PersistentProxyBuilder());
             IInterceptor interceptor = new WrapperClientInterceptor(typeof(TFunctions), targetArchitecture);
 
---
+++ LegacyWrapperTest/Client/EdgeCaseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using LegacyWrapperClient.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LegacyWrapperTest.Client
+{
+    [TestClass]
+    public class EdgeCaseTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestTypeIsNotAnInterface()
+        {
+            // Test a random class that's derrived from IDisposable but not an interface
+            using (WrapperClientFactory<HttpListener>.CreateWrapperClient())
+            {
+                // Do nothing
+            }
+        }
+    }
+}
